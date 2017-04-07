cce459e60f5a1bc2e17fff3a40e7cd6f3b26824e
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-04-07 09:04:22 -1000

    pseudo coded the div elements to change to next img, need to figure out actual code

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ function buttonone (){
 function buttontwo(){
   document.getElementById("but-b").style.backgroundColor="green";
 }
+
+function yes(){
+  /*
+  if click this yes div, change to next img
+  */
+}
+
+function no (){
+  /*
+  if click this no div, change to next img
+  */
+}
