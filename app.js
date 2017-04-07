0bfc52d1988265d1962754209d64ea2d68e398e4
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-04-07 08:54:18 -1000

    added a function to the second button

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ function buttonone (){
   document.getElementById("but-a").style.backgroundColor="red";
 }
 
+function buttontwo(){
+  document.getElementById("but-b").style.backgroundColor="green";
+}
