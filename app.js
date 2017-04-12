1c2356783bcb530a3095e80b4291e2faa2443d53
Braden Amorozo <braden808@gmail.com>
2017-04-12 09:22:29 -1000

    update log

---
+++ app.js
@@ -0,0 +1,27 @@
+var storedImages = [];
+
+ function randomizer() {
+ 	Math.floor(Math.random() * 10)
+ }
+
+ console.log(randomizer);
+
+ /*mock data*/
+ var mockData = [
+ 	{
+ 		Type:"Two Piece",
+ 		Design:"Floral-Print",
+ 		length:"Long"
+ 	},
+ 	{
+ 		Type:"Sleeveless"
+ 		length:"Floor-Length"
+ 		design:"Bead Embellished Bodice"
+ 	},
+ 	{
+ 		Type:"Strapless Sweetheart"
+ 		length:"Floor Length" 
+ 		Color:"Navy" 
+ 		Design:"Side Slit"
+ 	},
+ ];
