f2984674a77123555b89756fc2adec6d62115669
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-04-24 08:41:00 -1000

    fixed js mock data for both, no spaces needed.

--- app.js
+++ app.js
@@ -51,37 +51,37 @@ Function m
 
  var mockData = [
  	{
- 		Type:"Two Piece",
- 		Design:"Floral-Print",
- 		length:"Long"
-    Url: "https://www.promgirl.com/shop/dresses/viewitem-PD1561458"
+ 		Type: " Two Piece",
+ 		Design: " Floral-Print",
+ 		length: " Long"
+    Url: " https://www.promgirl.com/shop/dresses/viewitem-PD1561458"
  	},
  	{
- 		Type:"Sleeveless"
- 		length:"Floor-Length"
- 		Design:"Bead Embellished Bodice"
-    Url:"https://www.promgirl.com/shop/dresses/viewitem-PD1520500"
+ 		Type:" Sleeveless"
+ 		length:" Floor-Length"
+ 		Design:" Bead Embellished Bodice"
+    Url:" https://www.promgirl.com/shop/dresses/viewitem-PD1520500"
  	},
  	{
- 		Type:"Strapless Sweetheart"
- 		length:"Floor Length" 
- 		Color:"Navy" 
- 		Design:"Side Slit"
-    Url:"https://www.promgirl.com/shop/dresses/viewitem-PD1399092"
+ 		Type: " Strapless Sweetheart"
+ 		length: " Floor Length" 
+ 		Color: " Navy" 
+ 		Design: " Side Slit"
+    Url: " https://www.promgirl.com/shop/dresses/viewitem-PD1399092"
  	},
   {
-    Type: "Long"
-    Color:"Eggplant Purple" 
-    Event:"Prom Dress"
-    Design:"Beaded Bodice"
-    Url: "https://www.promgirl.com/shop/dresses/viewitem-PD1632253"
+    Type: " Long"
+    Color: " Eggplant Purple" 
+    Event: " Prom Dress"
+    Design: " Beaded Bodice"
+    Url: " https://www.promgirl.com/shop/dresses/viewitem-PD1632253"
   },
   {
-    Type:"Open-Back Illusion Long"
-    Event:"Prom Dress" 
-    Design:"Beaded Bodice"
-    Color: "Light Pink"
-    Url: "https://www.promgirl.com/shop/dresses/viewitem-PD1582814"
+    Type: " Open-Back Illusion Long"
+    Event: " Prom Dress" 
+    Design: " Beaded Bodice"
+    Color: " Light Pink"
+    Url: " https://www.promgirl.com/shop/dresses/viewitem-PD1582814"
   }
 
  ];
@@ -90,32 +90,32 @@ Function m
 
  var compareData = [
  	{
- 		name:"Waipahu High School",
- 		percent: "5%"
+ 		name: " Waipahu High School",
+ 		percent: " 5%"
  	},
  	{
- 		name: "Mckinley",
- 		percent: "2%"
+ 		name: " Mckinley",
+ 		percent: " 2%"
  	},
  	{
- 		name: "Radford",
- 		percent: "10%"
+ 		name: " Radford",
+ 		percent: " 10%"
  	},
  	{
- 		name: "Kalani",
- 		percent: "30%"
+ 		name: " Kalani",
+ 		percent: " 30%"
  	},
  	{
- 		name: "Roosevelt",
- 		percent: "10%"
+ 		name: " Roosevelt",
+ 		percent: " 10%"
  	},
  	{
- 		name: "Pearl City",
- 		percent: "13%"
+ 		name: " Pearl City",
+ 		percent: " 13%"
  	},
  	{
- 		name: "Waialua",
- 		percent: "30%"
+ 		name: " Waialua",
+ 		percent: " 30%"
  	}
  ];
 
