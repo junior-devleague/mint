7c6e755ac3b759a05e3d06deae5db1124912987a
Braden Amorozo <braden808@gmail.com>
2017-05-01 08:42:46 -1000

    fixing js

--- app.js
+++ app.js
@@ -121,15 +121,4 @@ console.log("Check");
  		percent: "30%"
  	}
  ];
-
- 
-var current = 0,
-    slides = document.getElementsByTagName("img");
-
-function changeImage() {
-  for (var i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0;
-  }
-  current = (current != slides.length - 1) ? current + 1 : 0;
-  slides[current].style.opacity = 1;
 }; 
