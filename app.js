a5ce731fc86b1dbab5c0daa6e2ede0903405d05f
Braden Amorozo <braden808@gmail.com>
2017-05-03 09:17:48 -1000

    added dank randomizer Braden & Isaiah

--- app.js
+++ app.js
@@ -1,89 +1,38 @@
-console.log("Check");
-
-// function buttonone (){
-//   document.getElementById("no").addEventListener();
-// }
-
-// function buttontwo(){
-//   document.getElementById("yes").addEventListener();
-// }
-// var storedNumbers = [];
-
-// var images = [/*dresses*/];
-// function yes(){
-//   document.getElementById("yes")
-//   /*
-//   here i put the randomizer when i getum from braden so the onlick function chooses from the array
-//   both yes and no function will have the same output
-//   */
-// } 
-
-// function no (){
-//   document.getElementById.("no")
-//   /*
-//   here i put the randomizer when i getum from braden so the onlick function chooses from the array
-//   both yes and no functions will have same result
-//   */
-// }
-
-// //Randomizer
-
-// var storedImages = [];
-// var random = Math.floor(Math.random() * 10)
-//  function randomizer() {
-//  	random
-//  }
-
-// storedImages.push(random)
- 
-//  console.log(randomizer);
-
-// function noSame() {
-// 	if (/*random number = to storedImages*/) {/*
-// 	Run Function Again*/}
-// }
-
-
- /*mock data voting page*/
-            
-
- /*mock data*/
-
- /*mock data voting page*/
+/*mock data voting page*/
 
  var mockData = [
  	{
  		Type:"Two Piece",
  		Design:"Floral-Print",
- 		length:"Long"
+ 		length:"Long",
     Url: "https://www.promgirl.com/shop/dresses/viewitem-PD1561458"
  	},
  	{
- 		Type:"Sleeveless"
- 		length:"Floor-Length"
- 		Design:"Bead Embellished Bodice"
+ 		Type:"Sleeveless",
+ 		length:"Floor-Length",
+ 		Design:"Bead Embellished Bodice",
     Url:"https://www.promgirl.com/shop/dresses/viewitem-PD1520500"
  	},
  	{
- 		Type:"Strapless Sweetheart"
- 		length:"Floor Length" 
- 		Color:"Navy" 
- 		Design:"Side Slit"
+ 		Type:"Strapless Sweetheart",
+ 		length:"Floor Length", 
+ 		Color:"Navy", 
+ 		Design:"Side Slit",
     Url:"https://www.promgirl.com/shop/dresses/viewitem-PD1399092"
  	},
   {
-    Type: "Long"
-    Color:"Eggplant Purple" 
-    Event:"Prom Dress"
-    Design:"Beaded Bodice"
+    Type: "Long",
+    Color:"Eggplant Purple", 
+    Event:"Prom Dress",
+    Design:"Beaded Bodice",
     Url: "https://www.promgirl.com/shop/dresses/viewitem-PD1632253"
 
   },
   {
-    Type:"Open-Back Illusion Long"
-    Event:"Prom Dress" 
-    Design:"Beaded Bodice"
-    Color: "Light Pink"
+    Type:"Open-Back Illusion Long",
+    Event:"Prom Dress", 
+    Design:"Beaded Bodice",
+    Color: "Light Pink",
     Url: "https://www.promgirl.com/shop/dresses/viewitem-PD1582814"
   }
 
@@ -121,4 +70,17 @@ console.log("Check");
  		percent: "30%"
  	}
  ];
-}; 
+
+var noSame [];
+
+var x = Math.floor((Math.random() * 5));
+
+function randomizer() {
+	console.log(x);
+};
+
+var images = ["https://img.promgirl.com/_img/PGPRODUCTS/1561458/1000/black-navy-dress-BL-11137-a.jpg", "https://img.promgirl.com/_img/PGPRODUCTS/1520500/1000/black-dress-FB-GL1322-d.jpg", "https://img.promgirl.com/_img/PGPRODUCTS/1399092/1000/navy-dress-MF-E1778-c.jpg", "https://img.promgirl.com/_img/PGPRODUCTS/1632253/1000/eggplant-dress-FB-GL2282-a.jpg", "https://img.promgirl.com/_img/PGPRODUCTS/1582814/1000/champagne-dress-DQ-9474-b.jpg"];
+
+function changeImage() {
+	 document.images["pic"].src = images[x];
+};
